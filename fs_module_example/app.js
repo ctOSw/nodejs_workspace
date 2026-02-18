b18bcd60f102d2a308dff460d802c24d7b163e0e
arda kaymak <158074668+ctOSw@users.noreply.github.com>
2026-02-18 23:26:59 +0300

    Add files via upload

---
+++ fs_module_example/app.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+const initialData = [
+  { id: 1, name: "Ahmet Yılmaz", position: "Geliştirici" },
+  { id: 2, name: "Ayşe Demir", position: "Tasarımcı" },
+];
+
+//   employees.json dosyası oluşturuldu ve içerik eklendi
+fs.writeFile("employees.json", JSON.stringify(initialData), "utf-8", (err) => {
+  if (err) throw err;
+
+  console.log(
+    "employees.json dosyası başarıyla oluşturuldu ve içerik eklendi.",
+  );
+});
+
+// dosyayı okuyalım
+fs.readFile("./employees.json", "utf-8", (err, data) => {
+  if (err) throw err;
+
+  console.log(`Dosya içeriği görüntüleniyor : ${data}`);
+});
+
+// dosyayı güncelleyeim
+fs.readFile("./employees.json", "utf-8", (err, data) => {
+  if (err) throw err;
+  let employees = JSON.parse(data);
+
+  employees = employees.map((emp) =>
+    emp.id === 1 ? { ...emp, position: "Kıdemli Geliştirici" } : emp,
+  );
+
+  fs.writeFile("employees.json", JSON.stringify(employees), "utf-8", (err) => {
+    if (err) throw err;
+    console.log("Ahmet Yılmaz'ın pozisyonu güncellendi.");
+  });
+});
+
+fs.unlink("./employees.json", (err) => {
+  if (err) throw err;
+  console.log("Dosya başarıyla silindi");
+});
